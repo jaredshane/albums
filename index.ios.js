24c6738eb22a4c082ed03757c539878b2734b6df
Jared Fuller <jaredshane@gmail.com>
2017-04-29 11:40:21 -0500

    required in header component and added that to the app component

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,11 @@
 //import a library to help create a component
 import React, { Component } from 'react'
-import { Text, AppRegistry } from 'react-native'
+import { AppRegistry } from 'react-native'
+import Header from './src/components/header'
 
 //create a Component
 
-const App = () => <Text>Hello</Text>
+const App = () => <Header />
 
 //render to the device
 AppRegistry.registerComponent('albums', () => App)
