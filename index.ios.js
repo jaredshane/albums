6475c4aad45b8448599fc95197819aff11fd95bc
Jared Fuller <jaredshane@gmail.com>
2017-04-29 11:01:13 -0500

    imported react and react-native and created a component to render to the phone

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,10 @@
+//import a library to help create a component
 import React, { Component } from 'react'
+import { Text, AppRegistry } from 'react-native'
+
+//create a Component
+
+const App = () => <Text>Hello</Text>
+
+//render to the device
+AppRegistry.registerComponent('albums', () => App)
