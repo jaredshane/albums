9121c3aaae243a9de52e4dcaeb90132cb73be101
Jared Fuller <jaredshane@gmail.com>
2017-05-01 08:35:06 -0500

    added some styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Text, View } from 'react-native'
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}> Albums </Text>
+      <Text style={textStyle}> {props.headerText} </Text>
     </View>
   )
-
+}
 
 const styles = {
   textStyle: {
     fontSize: 20
   },
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   }
 }
 
